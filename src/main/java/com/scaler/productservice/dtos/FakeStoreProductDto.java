1e8aec7234a9dd5174ec3e7edabe2b43a727a153
Juturu2021 <juturu2021@gmail.com>
2024-06-30 23:31:37 +0530

    Implementing product service and done with getProductById

---
+++ src/main/java/com/scaler/productservice/dtos/FakeStoreProductDto.java
@@ -0,0 +1,15 @@
+package com.scaler.productservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeStoreProductDto {
+    private  long id;
+    private String title;
+    private double price;
+    private String category;
+    private String desciption;
+    private String image;
+}
